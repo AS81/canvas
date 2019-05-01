b2567b0ac8f338bafdf5045a2f18acfdcc8c70c7
AS81 <standwww@gmail.com>
2019-05-01 22:13:06 +0300

    When turn off on 180 toDraw draw snake in 1 cell

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ function loop() {
   // eslint-disable-next-line
 
 
-  ctx.clearRect(0, 0, window.innerWidth, innerHeight);
+  ctx.clearRect(0, 0, innerWidth, innerHeight);
   ctx.beginPath();
   ctx.save();
   ctx.translate(offset, offset);
@@ -38,30 +38,41 @@ function loop() {
 
 window.requestAnimationFrame(loop);
 
-console.log("s1 " + snake.direction[0] + snake.direction[1]);
+let lastKeyCode = 40; //Snake watch down
+//console.log("s1 " + snake.direction[0] + snake.direction[1]);
 document.onkeydown = function (e) {
   firstDraw = 0;
-  console.log(`You ${snake.direction[0]}  ${snake.direction[1]}`);
+  //console.log(`You ${snake.direction[0]}  ${snake.direction[1]}`);
+
+  // Snake can't turn off 180 degree
 
   switch (e.keyCode) {
     case 37: //left
-      if (snake.direction[0] !== -wall) {
-        snake.direction[0] = snake.direction[0] - 1;
-      } else {
-        snake.direction[0] = wall;
+      console.log("key 37 " + lastKeyCode);
+      if (lastKeyCode != 39) {
+        console.log("make37");
+        if (snake.direction[0] !== -wall) {
+          snake.direction[0] = snake.direction[0] - 1;
+          lastKeyCode = 37;
+        } else {
+          snake.direction[0] = wall;
+        }
       }
       break;
     case 38: //up
       if (snake.direction[1] !== -wall) {
         snake.direction[1] = snake.direction[1] - 1;
+        lastKeyCode = 38;
       } else {
         snake.direction[1] = wall;
       }
       break;
     case 39: //right
+      console.log("key 39 " + lastKeyCode);
+      console.log("make39");
       if (snake.direction[0] !== wall) {
         snake.direction[0] = snake.direction[0] + 1;
-
+        lastKeyCode = 39;
       } else {
         snake.direction[0] = -wall;
       }
@@ -69,6 +80,7 @@ document.onkeydown = function (e) {
     case 40: //down
       if (snake.direction[1] !== wall) {
         snake.direction[1] = snake.direction[1] + 1;
+        lastKeyCode = 40;
       } else {
         snake.direction[1] = -wall;
       }
